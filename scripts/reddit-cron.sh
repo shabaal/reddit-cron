6be280ff9d40ef3b3c9eeef67f22c49807564a8d
shabaal <alexandre.montesso@yahoo.com>
2025-05-15 13:31:03 +0100

    add reddit-cron.sh

---
+++ scripts/reddit-cron.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -X POST "https://mnrnjybdpcswfzlwvjjt.functions.supabase.co/collect_reddit_trending" -H "Authorization: Bearer b99a71c9e2d4eb8900a349bc5b64d73a9e4ee2802e7d336b250b0a6a797482aa"
